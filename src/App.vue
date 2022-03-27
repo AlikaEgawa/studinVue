9d6f827d88dfc50f2cc8c50ee19dce1e1c8ad687
AlikaEgawa <akiya9n@gmail.com>
2022-03-27 21:47:01 +0900

    day2 started

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <LikeHeader>
+      <template v-slot:title>
+        <h2>
+          こんにちは
+        </h2>
+      </template>
+
+      <template v-slot:number>
+        <p>
+          {{ number }}
+        </p>
+      </template>
+
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
+    <LikeNumber :total-number="number"></LikeNumber>
   </div>
 </template>
 
@@ -9,8 +23,24 @@
 import LikeHeader from "./components/LikeHeader.vue";
 
 export default({
+  data() {
+    return {
+      number: 14
+    };
+  },
   components: {
     LikeHeader
-  }
+  },
+  methods: {
+    incrementNumber(value){
+      this.number = value
+    }
+  },
 })
 </script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>
