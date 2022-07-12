8c7c3e5852b64ca89bbb43eb458297c35d5e56e4
AlikaEgawa <akiya9n@gmail.com>
2022-07-12 12:27:31 +0900

    section10 #144

--- src/main.js
+++ src/main.js
@@ -4,23 +4,6 @@ import LikeNumber from "./components/LikeNumber.vue"
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive("border", {
-  bind() {
-    // ディレクティブが初めて対象の要素にひも付いた時
-  },
-  inserted() {
-    // 親Nodeに挿入された時
-  },
-  update() {
-    // コンポーネントが更新される度。子コンポーネントが更新される前
-  },
-  componentUpdated(){
-    // コンポーネントが更新される度。子コンポーネントが更新された後
-  },
-  unbind(){
-    // ディレクティブがひも付いている要素から取り除かれたとき
-  }
-});
 
 new Vue({
   render: h => h(App),
